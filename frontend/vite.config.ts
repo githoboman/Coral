499150e7cab831b03f66c28717f3c279930340d9
Mesal Okubor <mesalokubor63@gmail.com>
2025-09-27 11:41:57 +0100

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ["tovira.onrender.com", "localhost"],
+    allowedHosts: ["tovira.onrender.com", "localhost", "tovira.xyz"],
   },
 })
