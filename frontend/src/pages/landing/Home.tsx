fd98dcd7deb789b9fa0afe00a9fc5e78f86af497
Mesal Okubor <mesalokubor63@gmail.com>
2025-10-18 17:31:37 +0100

    fix deployemnt errors

--- frontend/src/pages/landing/Home.tsx
+++ frontend/src/pages/landing/Home.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { motion, Variants } from 'framer-motion';
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 interface Feature {
